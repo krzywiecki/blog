859beb4901f229ae07b54e7f873fa0c725497ccf
Dominik <dominik@krzywiecki.com>
2016-02-04 03:57:41 +0100

    Implement index.html template

---
+++ web/js/app.js
@@ -0,0 +1,14 @@
+var app = new App();
+
+jQuery(document).ready(function($) {
+  app.init();
+});
+
+function App() {
+  var _selft = this;
+
+  this.init = function() {
+    console.log('Hello world');
+  }
+
+}
